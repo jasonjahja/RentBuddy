d6281d6c9e125a8e01ee084cfb2e2116ffcf2b81
jasonjahja <jasonjahja@gmail.com>
2024-12-08 20:27:59 +0700

    can deploy

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,20 +1,16 @@
-import type { DefaultJWT } from "next-auth";
+import type { DefaultSession, DefaultJWT } from "next-auth";
 
 declare module "next-auth" {
   // Extend Session object
-  interface Session {
+  interface Session extends DefaultSession {
     user: {
-      id: string; // Include id
-      email?: string | null;
-      name?: string | null;
-      image?: string | null;
-    };
+      id: string; // Add id
+    } & DefaultSession["user"];
   }
 
   // Extend JWT object
   interface JWT extends DefaultJWT {
     id: string; // Include id in the token
-    email?: string | null;
   }
 
   // Extend User object
