f1101460bd75661e2658dc031c36a8c3a500206c
jasonjahja <jasonjahja@gmail.com>
2024-12-08 19:47:58 +0700

    fixed dependancy 5

--- src/app/components/ui/FloatingNavbar.tsx
+++ src/app/components/ui/FloatingNavbar.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useState } from "react";
 import {
   motion,
@@ -18,7 +16,6 @@ export const FloatingNav = ({
   navItems: {
     name: string;
     link: string;
-    icon?: JSX.Element;
   }[];
   className?: string;
 }) => {
@@ -37,7 +34,7 @@ export const FloatingNav = ({
   });
 
   return (
-    <AnimatePresence mode="wait">
+    <AnimatePresence>
       <motion.div
         initial={{ opacity: 1, y: -100 }}
         animate={{ y: visible ? 0 : -100, opacity: visible ? 1 : 0 }}
@@ -45,7 +42,7 @@ export const FloatingNav = ({
         className={cn(
           "fixed top-0 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-8 py-2 flex items-center flex-wrap",
           className
-        )}
+        )} 
       >
         {/* Navigation items */}
         <div className="ml-auto flex items-center space-x-4">
@@ -57,7 +54,6 @@ export const FloatingNav = ({
                 "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
               )}
             >
-              <span className="block sm:hidden">{navItem.icon}</span>
               <span className="hidden sm:block text-sm">{navItem.name}</span>
             </Link>
           ))}
