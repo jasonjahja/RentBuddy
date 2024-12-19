72f1db1774a96f0341a5298ec56f3311084becdf
jasonjahja <jasonjahja@gmail.com>
2024-12-19 15:39:05 +0700

    fixed deployment issue: review

--- src/app/api/reviews/route.ts
+++ src/app/api/reviews/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
 if (process.env.NODE_ENV === "production") {
